afe7c55278ab479159beeb9620fd809fe2c02f05
BRAINCaen <brain.caen@gmail.com>
2025-06-18 20:19:30 +0200

    Update firebase-manager.js

--- src/core/firebase-manager.js
+++ src/core/firebase-manager.js
@@ -23,11 +23,12 @@ const firebaseConfig = {
   apiKey: "AIzaSyD7uBuAQaOhZ02owkZEuMKC5Vji6PrB2f8",
   authDomain: "synergia-app-f27e7.firebaseapp.com",
   projectId: "synergia-app-f27e7",
-  storageBucket: "synergia-app-f27e7.firebasestorage.app", // ⚠️ Correction potentielle
+  storageBucket: "synergia-app-f27e7.appspot.com", // ← c'est ça !
   messagingSenderId: "201912738922",
   appId: "1:201912738922:web:2fcc1e49293bb632899613",
   measurementId: "G-EGJ79SCMWX"
 };
+
 
 
 
