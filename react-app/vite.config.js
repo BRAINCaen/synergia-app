18691a0f7dd4126cb2456b427a6e1f52bf4b4e41
BRAINCaen <brain.caen@gmail.com>
2025-06-19 20:59:04 +0200

    Update vite.config.js

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  root: '.', // ou supprimez cette ligne si index.html est à la racine
+  build: {
+    outDir: 'dist'
+  }
+})
