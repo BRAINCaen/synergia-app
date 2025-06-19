dc81836720768ab7008a74e50505868cd2f4a95f
BRAINCaen <brain.caen@gmail.com>
2025-06-19 21:44:42 +0200

    Update index.jsx

--- react-app/src/index.jsx
+++ react-app/src/index.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { registerSW, setupPWAInstall } from "./utils/pwa";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+// Enregistrement du service worker et configuration PWA
+registerSW();
+setupPWAInstall();
