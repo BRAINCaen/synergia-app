c4ab7bde6ae644f1e031c6444c8aec1e17d2bcdb
BRAINCaen <brain.caen@gmail.com>
2025-06-19 23:13:47 +0200

    Create index.js

---
+++ react-app/src/shared/components/ui/index.js
@@ -0,0 +1,13 @@
+export { default as Button } from './Button'
+export { default as Input } from './Input'
+export { default as Card, StatCard } from './Card'
+export { default as Modal, ConfirmModal } from './Modal'
+export { 
+  default as Loading, 
+  Spinner, 
+  LoadingPage, 
+  CardSkeleton, 
+  ListSkeleton, 
+  TableSkeleton, 
+  InlineLoading 
+} from './Loading'
