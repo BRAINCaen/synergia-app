69688c03387dbe6f5da46efc5393693a04d68766
BRAINCaen <brain.caen@gmail.com>
2025-06-22 17:05:17 +0200

    Update Login.jsx

--- react-app/src/pages/Login.jsx
+++ react-app/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Login from '../modules/auth/components/Login.jsx'
+import Login from '../modules/auth/Login.jsx'
 
 export default function LoginPage() {
   return <Login />
