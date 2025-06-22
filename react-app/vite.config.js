774df9acb07078ae464a6a3af1bf2bea42ca884f
BRAINCaen <brain.caen@gmail.com>
2025-06-22 15:57:56 +0200

    Update vite.config.js

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  server: {
+    port: 3000
   }
 })
