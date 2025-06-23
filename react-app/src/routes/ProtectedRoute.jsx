9367e4dc1db8028b98e999da01b4da9d1ebad263
BRAINCaen <brain.caen@gmail.com>
2025-06-24 00:51:41 +0200

    Update ProtectedRoute.jsx

--- react-app/src/routes/ProtectedRoute.jsx
+++ react-app/src/routes/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 // src/routes/ProtectedRoute.jsx
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
-import useAuthStore from '../shared/stores/authStore.js';
+import { useAuthStore } from '../shared/stores/authStore.js';
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuthStore();
