ba596fcdbcc1aa8bc466819437383c440b604ffb
BRAINCaen <brain.caen@gmail.com>
2025-06-24 00:28:57 +0200

    Create TeamPage.jsx

---
+++ react-app/src/pages/TeamPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeamDashboard from '../components/team/TeamDashboard';
+
+const TeamPage = () => {
+  return (
+    <div className="w-full">
+      <TeamDashboard />
+    </div>
+  );
+};
+
+export default TeamPage;
