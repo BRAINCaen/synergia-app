39ad0e67d52a03dc9c07869f57789d874bad7531
BRAINCaen <brain.caen@gmail.com>
2025-06-24 03:50:12 +0200

    Create index.js

---
+++ react-app/src/shared/stores/index.js
@@ -0,0 +1,6 @@
+// react-app/src/shared/stores/index.js
+export { useAuthStore } from './authStore.js';
+export { useTaskStore } from './taskStore.js';
+export { useProjectStore } from './projectStore.js';
+export { useTeamStore, useTeamData, useTeamFilters, useTeamStats, useTeamActivities } from './teamStore.js';
+export { useGamificationStore } from './gamificationStore.js';
