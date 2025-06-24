676462d0ceb8e1f02d6163583409d0d141da514d
BRAINCaen <brain.caen@gmail.com>
2025-06-24 03:50:49 +0200

    Create index.js

---
+++ react-app/src/core/services/index.js
@@ -0,0 +1,6 @@
+// react-app/src/core/services/index.js
+export { default as authService } from './authService.js';
+export { default as taskService } from './taskService.js';
+export { default as projectService } from './projectService.js';
+export { default as teamService } from './teamService.js';
+export { default as gamificationService } from './gamificationService.js';
