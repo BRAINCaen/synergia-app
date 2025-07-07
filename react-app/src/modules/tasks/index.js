611233a86b38454150a4e9185561d0efbbd8525a
BRAINCaen <brain.caen@gmail.com>
2025-07-07 18:50:59 +0200

    Create index.js

---
+++ react-app/src/modules/tasks/index.js
@@ -0,0 +1,7 @@
+// ==========================================
+// 📁 react-app/src/modules/tasks/index.js
+// EXPORTS DES COMPOSANTS TASKS
+// ==========================================
+
+export { default as TaskForm } from './TaskForm.jsx';
+export { default as TaskCard } from './TaskCard.jsx';
