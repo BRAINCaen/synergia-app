9dcee69da4f25d52ff43aee3037459e03b0bb273
BRAINCaen <brain.caen@gmail.com>
2025-07-15 00:12:47 +0200

    Update GamificationPage.jsx

--- react-app/src/pages/GamificationPage.jsx
+++ react-app/src/pages/GamificationPage.jsx
@@ -207,7 +207,13 @@ const GamificationPage = () => {
           className="relative bg-gradient-to-br from-purple-600/20 via-blue-600/20 to-purple-800/20 rounded-2xl p-8 border border-purple-500/30 overflow-hidden"
         >
           {/* Effets de fond */}
-          <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width=\"60\" height=\"60\" viewBox=\"0 0 60 60\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cg fill=\"none\" fill-rule=\"evenodd\"%3E%3Cg fill=\"%239C92AC\" fill-opacity=\"0.1\"%3E%3Ccircle cx=\"30\" cy=\"30\" r=\"2\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] opacity-30"></div>
+          <div className="absolute inset-0 opacity-30">
+            <div className="absolute inset-0 bg-gradient-to-br from-purple-500/10 to-blue-500/10"></div>
+            <div className="absolute inset-0" style={{
+              backgroundImage: `url("data:image/svg+xml,${encodeURIComponent('<svg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><g fill="#9C92AC" fill-opacity="0.1"><circle cx="30" cy="30" r="2"/></g></g></svg>')}")`,
+              backgroundSize: '60px 60px'
+            }}></div>
+          </div>
           
           <div className="relative z-10">
             {/* Titre */}
