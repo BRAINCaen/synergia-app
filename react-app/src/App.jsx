4e200682cb27164a8af3c6439532c20fe7dc5a2f
BRAINCaen <brain.caen@gmail.com>
2025-08-09 12:15:22 +0200

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -14,6 +14,7 @@ import { useAuthStore, initializeAuthStore } from './shared/stores/authStore.js'
 
 // Import des correctifs
 import './utils/safeFix.js';
+import './utils/productionErrorPatch.js';
 
 // ==========================================
 // 📄 PAGES PRINCIPALES EXISTANTES
